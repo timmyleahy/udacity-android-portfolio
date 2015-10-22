eb94c5310c82f64aaa90e7a7f4aa5a7175518c4e
Tim Leahy <tleahy@google.com>
2015-10-22 15:04:21 -0700

    Project 0: adding buttons for later apps with interim toast message

--- app/src/main/java/com/timmyleahy/udacity/android/portfolio/MainActivityFragment.java
+++ app/src/main/java/com/timmyleahy/udacity/android/portfolio/MainActivityFragment.java
@@ -1,10 +1,12 @@
 package com.timmyleahy.udacity.android.portfolio;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +19,24 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        for (int buttonId :
+                new int[]{R.id.spotifyStreamerButton, R.id.superDuoButton, R.id.buildItBiggerButton,
+                        R.id.xyzReaderButton, R.id.capstoneButton}) {
+            final Button buttonView = (Button) rootView.findViewById(buttonId);
+            buttonView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getActivity(), String.format(getString(R.string.toastMessage), buttonView.getText()),
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        return rootView;
     }
+
+
 }
